2a6758e436937782998338d5f48961d753739289
rakeshdhundhara <rakeshdhundhara1997@gmail.com>
2023-09-01 11:01:27 +0530

    change

---
+++ veriable.tf
@@ -0,0 +1,18 @@
+variable "var.rakesh01" {
+type = string
+description = "rakesh"
+default = "content are change with variable"
+
+}
+variable "kesh" {
+type = bool
+description = "rakesh"
+default = True
+
+}
+variable "rak1" {
+type = any
+description = "rakesh"
+default = 123
+
+}
